e42dd766fe143ed278497861c624cb821e1ea37c
NONATO <tdvctoin@gmail.com>
2026-06-02 10:49:51 -0300

    Corrigir cadastro de empresa no admin

--- empresas/admin.py
+++ empresas/admin.py
@@ -1,24 +1,63 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import admin
+from django import forms
 
 from .models import Empresa, UsuarioEmpresa
 
 
+class EmpresaAdminForm(forms.ModelForm):
+    class Meta:
+        model = Empresa
+        fields = (
+            "cnpj",
+            "razao_social",
+            "nome_fantasia",
+            "ativo",
+        )
+
+
 @admin.register(Empresa)
 class EmpresaAdmin(admin.ModelAdmin):
-    list_display = ("razao_social", "nome_fantasia", "cnpj", "ativo", "criado_em")
-    search_fields = ("razao_social", "nome_fantasia", "cnpj")
-    list_filter = ("ativo",)
+    form = EmpresaAdminForm
 
-    def get_readonly_fields(self, request, obj=None):
-        if obj:
-            return ("token_api", "criado_em")
-        return ()
+    list_display = (
+        "id",
+        "razao_social",
+        "nome_fantasia",
+        "cnpj",
+        "ativo",
+    )
+
+    search_fields = (
+        "razao_social",
+        "nome_fantasia",
+        "cnpj",
+    )
+
+    list_filter = (
+        "ativo",
+    )
 
 
 @admin.register(UsuarioEmpresa)
 class UsuarioEmpresaAdmin(admin.ModelAdmin):
-    list_display = ("usuario", "empresa", "perfil", "ativo")
-    search_fields = ("usuario__email", "usuario__username", "empresa__razao_social", "empresa__cnpj")
-    list_filter = ("perfil", "ativo")
+    list_display = (
+        "id",
+        "usuario",
+        "empresa",
+        "perfil",
+        "ativo",
+    )
+
+    search_fields = (
+        "usuario__username",
+        "usuario__email",
+        "empresa__razao_social",
+        "empresa__cnpj",
+    )
+
+    list_filter = (
+        "perfil",
+        "ativo",
+    )
