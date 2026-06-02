657ae2067c3b4e85154b70ffcffd87867ec0eac5
NONATO <tdvctoin@gmail.com>
2026-06-02 11:07:51 -0300

    Mostrar token da empresa no admin

--- empresas/admin.py
+++ empresas/admin.py
@@ -1,26 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import admin
-from django import forms
 
 from .models import Empresa, UsuarioEmpresa
 
 
-class EmpresaAdminForm(forms.ModelForm):
-    class Meta:
-        model = Empresa
-        fields = (
-            "cnpj",
-            "razao_social",
-            "nome_fantasia",
-            "ativo",
-        )
-
-
 @admin.register(Empresa)
 class EmpresaAdmin(admin.ModelAdmin):
-    form = EmpresaAdminForm
-
     list_display = (
         "id",
         "razao_social",
@@ -38,6 +24,33 @@ class EmpresaAdmin(admin.ModelAdmin):
     list_filter = (
         "ativo",
     )
+
+    def get_fields(self, request, obj=None):
+        if obj:
+            return (
+                "cnpj",
+                "razao_social",
+                "nome_fantasia",
+                "ativo",
+                "token_api",
+                "criado_em",
+            )
+
+        return (
+            "cnpj",
+            "razao_social",
+            "nome_fantasia",
+            "ativo",
+        )
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return (
+                "token_api",
+                "criado_em",
+            )
+
+        return ()
 
 
 @admin.register(UsuarioEmpresa)
